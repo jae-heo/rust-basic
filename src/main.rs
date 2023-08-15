cbac7088d00b92c096bd0c16cb8933183a99a9de
Jae <gjwodud119@gmail.com>
2023-08-15 14:45:45 +0900

    Temperature Converting Feature Added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter your celsius degree to convert to farenheit.");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed in read_line");
+
+    let celsius : f32 = input.trim().parse().expect("Fail to parse. Your input is not in format of integer");
+
+    println!("Converted value of celsius to farenheit is : {}", ((celsius * 1.8) + 32.0));
 }
