4e8716f76e40d00091b133f4e7a47f0c16a2acd5
Jae <gjwodud119@gmail.com>
2023-08-15 17:40:47 +0900

    온도 변환 기능 함수로 분리

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::io;
 
 fn main() {
-    println!("Please enter your celsius degree to convert to farenheit.");
+    run_temperature_conversion()
+}
 
+fn run_temperature_conversion() {
+    println!("Please enter your celsius degree to convert to farenheit.");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed in read_line");
-
+    
     let celsius : f32 = input.trim().parse().expect("Fail to parse. Your input is not in format of integer");
-
     println!("Converted value of celsius to farenheit is : {}", ((celsius * 1.8) + 32.0));
 }
