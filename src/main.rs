8c7a26158fcde048d6e4d3d23f80cc5eef8ceac7
jay <gjwodud119@gmail.com>
2023-08-29 10:29:50 +0900

    The conclusion for modulization added

--- src/main.rs
+++ src/main.rs
@@ -18,4 +18,7 @@ As Modules are included in root, the crate contains the module.
 The crate has two kinds, binary/library
 
 Still confusing how should I divide program into modules, crates and packages.
+
+As our main function does not change even if we divide modules into saperated files.
+This means that we can dynamically chagne the file structure without change the paths (because it depends on module tree)
 */
