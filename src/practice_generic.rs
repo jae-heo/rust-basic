4553137da16c4bb707b2fccf52ef5316f327bd02
jay <gjwodud119@gmail.com>
2023-09-06 12:04:09 +0900

    Additional description for generic added.

--- src/practice_generic.rs
+++ src/practice_generic.rs
@@ -1,3 +1,23 @@
+/**
+ * When using generic, the performance does not lower in running time. It is because the code will be duplicated in compile time.
+ * for example, when we have code like 
+ * pub struct Point<T> {
+ *    pub x : T,
+ *    pub y : T,
+ * }
+ * 
+ * And in the code we use two kinds of point, Point<i32> and Point<f64>, the the code will be like..
+ * pub struct Point<i32> {
+ *    pub x : i32,
+ *    pub y : i32,
+ * }
+ * pub struct Point<f64> {
+ *    pub x : f64,
+ *    pub y : f64,
+ * }
+ * 
+ * Because this work is done in compile time, it does not effect runtime performance.
+ */
 pub struct Point<T> {
     pub x : T,
     pub y : T,
@@ -19,3 +39,4 @@ impl<T, U> Point2<T, U> {
         Point2 {x : self.x, y : other.y}
     }
 }
+
